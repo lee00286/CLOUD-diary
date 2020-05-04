cadfa7419a75e10282e052e36dee28cdcc62b46a
Yena Lee <lee00286@naver.com>
2020-05-04 17:27:19 +0900

    FourthViewController * Create new file called "FourthDetailViewController" * Connect class with textView

---
+++ CLOUD 1/FourthDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FourthDetailViewController.swift
+//  CLOUD 1
+//
+//  Created by Lee Yena on 2020-05-04.
+//  Copyright © 2020 Butter. All rights reserved.
+//
+
+import UIKit
+
+class FourthDetailViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textView.text = "New Note"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
