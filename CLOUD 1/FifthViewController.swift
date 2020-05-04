da6f7c8f0df1746204e79be50e489e1c5ac77f30
Yena Lee <lee00286@naver.com>
2020-05-04 16:39:22 +0900

    Edit Main.storyboard * Add view controller files and connect class with respective view controller

---
+++ CLOUD 1/FifthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FifthViewController.swift
+//  CLOUD 1
+//
+//  Created by Lee Yena on 2020-05-04.
+//  Copyright © 2020 Butter. All rights reserved.
+//
+
+import UIKit
+
+class FifthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
