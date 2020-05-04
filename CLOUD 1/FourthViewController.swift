edadbd836243c4afb24c78e60487e7b70d7ddbfb
Yena Lee <lee00286@naver.com>
2020-05-04 16:51:47 +0900

    Edit Main.storyboard * FourthViewController: Set data source for table

--- CLOUD 1/FourthViewController.swift
+++ CLOUD 1/FourthViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class FourthViewController: UIViewController {
-
+class FourthViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var table: UITableView!
+    var data:[String] = ["Item 1", "Item 2", "Item 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        table.dataSource = self
+    }
+    
+    // Number of rows in section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    // Cell for row in a IndexPath
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
     
 
