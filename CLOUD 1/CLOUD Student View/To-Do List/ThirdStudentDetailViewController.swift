fae5c06c6798447e966fc3dbcf4fc89a7c4a8080
Yena Lee <lee00286@naver.com>
2020-05-17 02:42:35 +0900

    To-Do Page

---
+++ CLOUD 1/CLOUD Student View/To-Do List/ThirdStudentDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdStudentDetailViewController.swift
+//  CLOUD 1
+//
+//  Created by Lee Yena on 2020-05-16.
+//  Copyright © 2020 Butter. All rights reserved.
+//
+
+import UIKit
+
+class ThirdStudentDetailViewController: UIViewController {
+    @IBOutlet weak var titleToDo: UITextField!
+    @IBOutlet weak var typeToDo: UIPickerView!
+    @IBOutlet weak var courseToDo: UITextField!
+    @IBOutlet weak var detailToDo: UITextField!
+    @IBOutlet weak var priorityToDo: UITextField!
+    @IBOutlet weak var duedateToDo: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
